1d91070bcb1e7d6ce1b8e393ecfa3d9d8191bd86
msyfls123 <msyfls123@gmail.com>
2018-01-27 23:33:36 +0800

    test split whitespace and parse to int

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 extern crate learn;
-use learn::url;
-use learn::print;
+// use learn::url;
+// use learn::print;
 // use learn::thread;
 
 fn main() {
-    url::run();
-    print::run();
+    // url::run();
+    // print::run();
     // thread::process();
+    let iter = "123 123 321".split_whitespace();
+    let data = iter.map(|x| x.to_string().parse::<i32>().unwrap()).collect::<Vec<i32>>();
+    println!("{:?}", data);
 }
