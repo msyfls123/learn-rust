3c6299961f6c52bf18280f0e2428e91aa4efc397
msyfls123 <msyfls123@gmail.com>
2020-01-25 21:12:22 +0800

    adventofcode 2017-13 part 2

--- advent_of_code/src/aoc2017/day13.rs
+++ advent_of_code/src/aoc2017/day13.rs
@@ -10,22 +10,34 @@ fn pos(x: usize, y: usize) -> usize {
   }
 }
 
+fn get_severity(pairs: &Vec<Vec<usize>>, delay: usize) -> usize {
+  pairs.iter().fold(
+    0,
+    |mut acc, pair| {
+      if let [depth, range] = &pair[..] {
+        if pos(*range, *depth + delay) == 0 {
+          acc += range * depth;
+        }
+      }
+      acc
+    }
+  )
+}
+
 fn main() {
   let array = get_str_array_from_file(&vec!{"aoc2017", "day13_data.txt"});
   let depth_range_pairs: Vec<Vec<usize>> = array.iter().map(
     |x| x.split(": ").map(|y| y.parse::<usize>().unwrap()).collect()
   ).collect();
   println!("{:?}", depth_range_pairs);
-  let severity = depth_range_pairs.iter().fold(
-    0,
-    |mut acc, pair| {
-      if let [depth, range] = &pair[..] {
-        if pos(*range, *depth) == 0 {
-          acc += range * depth;
-        }
-      }
-      acc
-    }
-  );
+  let severity = get_severity(&depth_range_pairs, 0);
   println!("Part 1: {}", severity);
+  let fewest_delay = (0..).find(|&delay| {
+    let first_got_caught = match &depth_range_pairs.first().unwrap()[..] {
+        [depth, range] => pos(*range, *depth + (delay as usize)) == 0,
+        _ => false
+    };
+    get_severity(&depth_range_pairs, delay as usize) == 0 && !first_got_caught
+  }).unwrap();
+  println!("Part 2: {}", fewest_delay);
 }
