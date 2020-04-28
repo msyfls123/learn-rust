782781ea2738befb4f0f4dc8255159f37d074e69
msyfls123 <msyfls123@gmail.com>
2020-04-28 08:52:11 +0800

    add day21 memo

---
+++ advent_of_code/src/aoc2017/day21.rs
@@ -0,0 +1,11 @@
+// 1. 设定俩 strategy struct Two & Three 
+// 2. 按行读取文件装入 strategy 列表
+// 3. 设定判断相同 pattern 的 trait（周边一圈数字正向反向循环找相等）
+// 4. 设定初始 pattern
+// 5. 根据 pattern 被 2 的整除结果 n，将其变成 n * n 个二维数组
+// 6. 每个二维数组通过匹配 strategy 转化成对应的 pattern
+// 7. 重复 5，6 步 5 次得到最终 pattern
+
+fn main() {
+    println!("Nothing!")
+}
