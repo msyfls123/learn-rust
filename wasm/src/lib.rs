f3cc8d6688d521a5be8988e26d9e3d83194a1624
msyfls123 <msyfls123@gmail.com>
2020-05-07 23:47:02 +0800

    sync html with state

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -2,14 +2,85 @@
 
 use wasm_bindgen::prelude::*;
 use web_sys;
-use web_sys::MouseEvent;
 
 use css_rs_macro::css;
 use virtual_dom_rs::prelude::*;
 
+use std::cell::{RefCell, Cell};
+use std::rc::Rc;
+
+// Expose globals from JS for things such as request animation frame
+// that web sys doesn't seem to have yet
+//
+// TODO: Remove this and use RAF from Rust
+// https://rustwasm.github.io/wasm-bindgen/api/web_sys/struct.Window.html#method.request_animation_frame
+#[wasm_bindgen]
+extern "C" {
+    pub type GlobalJS;
+
+    pub static global_js: GlobalJS;
+
+    #[wasm_bindgen(method)]
+    pub fn update(this: &GlobalJS);
+}
+
+struct Store {
+  click_count: Rc<Cell<u32>>,
+}
+
+impl Store {
+  fn click_count(&self) -> u32 {
+    self.click_count.get()
+  }
+  fn increment_click(&mut self) {
+    web_sys::console::log_1(&"Updating state".into());
+    global_js.update();
+    self.click_count.set(self.click_count.get() + 1);
+  }
+}
+
+pub struct HomeView {
+  store: Rc<RefCell<Store>>,
+}
+
+impl HomeView {
+    fn new(store: Rc<RefCell<Store>>) -> HomeView {
+      HomeView { store }
+    }
+}
+
+impl View for HomeView {
+  fn render(&self) -> VirtualNode {
+    let store = Rc::clone(&self.store);
+
+    let click_count = self.store.borrow().click_count();
+    let click_count = &*click_count.to_string();
+    html! {
+      // Use regular Rust comments within your html
+      <div class="big blue">
+      /* Interpolate values using braces */
+      <strong>{ click_count }</strong>
+      
+      <button
+        class=MY_COMPONENT_CSS
+        onclick=move |_event: web_sys::Event| {
+          web_sys::console::log_1(&"Button Clicked!".into());
+          store.borrow_mut().increment_click();
+        }
+      >
+      // No need to wrap text in quotation marks (:
+        Click me and check your console
+      </button>
+      </div>
+    }
+  }
+}
+
+
 #[wasm_bindgen]
 struct App {
-  dom_updater: DomUpdater
+  dom_updater: DomUpdater,
+  view: HomeView
 }
 
 #[wasm_bindgen]
@@ -21,32 +92,17 @@ impl App {
         let window = web_sys::window().unwrap();
         let document = window.document().unwrap();
         let body = document.body().unwrap();
-
+        let store = Rc::new(RefCell::new(Store {click_count: Rc::new(Cell::new(0))}));
         let mut dom_updater = DomUpdater::new_append_to_mount(start_view, &body);
 
-        let greetings = "Hello, World!";
+        let view = HomeView::new(store);
 
-        let end_view = html! {
-           // Use regular Rust comments within your html
-           <div class="big blue">
-              /* Interpolate values using braces */
-              <strong>{ greetings }</strong>
+        dom_updater.update(view.render());
 
-              <button
-                class=MY_COMPONENT_CSS
-                onclick=|_event: MouseEvent| {
-                   web_sys::console::log_1(&"Button Clicked!".into());
-                }
-              >
-                // No need to wrap text in quotation marks (:
-                Click me and check your console
-              </button>
-           </div>
-        };
-
-        dom_updater.update(end_view);
-
-        App { dom_updater }
+        App { dom_updater, view }
+    }
+    pub fn render(&mut self) {
+      self.dom_updater.update(self.view.render());
     }
 }
 
