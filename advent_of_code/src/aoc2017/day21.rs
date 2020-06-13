f446996b60ccccb519c7d4892a388a01260b1464
msyfls123 <msyfls123@gmail.com>
2020-06-13 22:40:59 +0800

    aoc2017-21: add data and parse part

--- advent_of_code/src/aoc2017/day21.rs
+++ advent_of_code/src/aoc2017/day21.rs
@@ -7,41 +7,129 @@
 // 7. 重复 5，6 步 5 次得到最终 pattern
 
 use std::convert::TryInto;
+use advent_of_code::get_str_array_from_file;
+
+static T: bool = true;
+static F: bool = false;
+
+static TWO_INDEX: [[usize; 2]; 4] = [[0, 0], [0, 1], [1, 1], [1, 0]];
+static THREE_INDEX: [[usize; 2]; 8] = [
+    [0, 0],
+    [0, 1],
+    [0, 2],
+    [1, 2],
+    [2, 2],
+    [2, 1],
+    [2, 0],
+    [1, 0],
+];
+
+type TwoDimension = [[bool; 2]; 2];
+type ThreeDimension = [[bool; 3]; 3];
+type FourDimension = [[bool; 4]; 4];
 
 trait Match {
     type Item;
     fn compare(&self, item: Self::Item) -> bool;
 }
 
-static T: bool = true;
-static F: bool = false;
-static TWO_INDEX: [[usize; 2]; 4] = [[0, 0], [0, 1], [1, 1], [1, 0]];
-
-type TwoDimension = [[bool; 2]; 2];
+fn compare_bool_vectors(vec1: &Vec<bool>, vec2: &Vec<bool>) -> bool {
+    let len: isize = vec1.iter().count().try_into().unwrap(); 
+    let mut result = false;
+    for j in [-1, 1].iter() {
+        for i in 0..len {
+            result |= vec1.iter().enumerate().all(|(k, &v)| {
+                let index = (((k as isize) * j + (i as isize) + len) % len) as usize;
+                v == vec2[index]
+            });
+            if result {
+                return result;
+            }
+        };
+    };
+    result
+}
 
 #[derive(Debug)]
 struct Two {
     pattern: TwoDimension,
-    result: [[bool; 3]; 3],
+    result: ThreeDimension,
+}
+
+impl Two {
+    fn new(item: &Vec<Vec<Vec<bool>>>) -> Self {
+        let mut pattern: TwoDimension = [[F; 2]; 2];
+        let mut result: ThreeDimension = [[F; 3]; 3];
+        for (row1, row2) in pattern.iter_mut().zip(item[0].iter()) {
+            for (column1, column2) in row1.iter_mut().zip(row2.iter()) {
+                *column1 = *column2;
+            };
+        };
+        for (row1, row2) in result.iter_mut().zip(item[1].iter()) {
+            for (column1, column2) in row1.iter_mut().zip(row2.iter()) {
+                *column1 = *column2;
+            };
+        };
+        Two {
+            pattern,
+            result,
+        }
+    }
 }
 
 impl Match for Two {
-  type Item = TwoDimension;
-  fn compare(&self, item: Self::Item) -> bool {
-    let circle: Vec<bool> = TWO_INDEX.iter().map(|&v| self.pattern[v[0]][v[1]]).collect();
-    let circle2: Vec<bool> = TWO_INDEX.iter().map(|&v| item[v[0]][v[1]]).collect();
-    let len: isize = circle.iter().count().try_into().unwrap();
-    let mut result = false;
-    for j in [-1, 1].iter() {
-        for i in 0..len {
-            result |= circle.iter().enumerate().all(|(k, &v)| {
-                let index = (((k as isize) * j + (i as isize) + len) % len) as usize;
-                v == circle2[index]
-            });
+    type Item = TwoDimension;
+    fn compare(&self, item: Self::Item) -> bool {
+        let circle: Vec<bool> = TWO_INDEX.iter().map(|&v| self.pattern[v[0]][v[1]]).collect();
+        let circle2: Vec<bool> = TWO_INDEX.iter().map(|&v| item[v[0]][v[1]]).collect();
+        compare_bool_vectors(&circle, &circle2)
+    }
+}
+
+#[derive(Debug)]
+struct Three {
+    pattern: ThreeDimension,
+    result: FourDimension,
+}
+
+impl Three {
+    fn new(item: &Vec<Vec<Vec<bool>>>) -> Self {
+        let mut pattern: ThreeDimension = [[F; 3]; 3];
+        let mut result: FourDimension = [[F; 4]; 4];
+        for (row1, row2) in pattern.iter_mut().zip(item[0].iter()) {
+            for (column1, column2) in row1.iter_mut().zip(row2.iter()) {
+                *column1 = *column2;
+            };
         };
-    };
-    result
-  }
+        for (row1, row2) in result.iter_mut().zip(item[1].iter()) {
+            for (column1, column2) in row1.iter_mut().zip(row2.iter()) {
+                *column1 = *column2;
+            };
+        };
+        Three {
+            pattern,
+            result,
+        }
+    }
+}
+
+impl Match for Three {
+    type Item = ThreeDimension;
+    fn compare(&self, item: Self::Item) -> bool {
+        if self.pattern[1][1] != item[1][1] {
+            false
+        } else {
+            let circle: Vec<bool> = THREE_INDEX.iter().map(|&v| self.pattern[v[0]][v[1]]).collect();
+            let circle2: Vec<bool> = THREE_INDEX.iter().map(|&v| item[v[0]][v[1]]).collect();
+            compare_bool_vectors(&circle, &circle2)
+        }
+    }
+}
+
+#[derive(Debug)]
+enum Rule {
+    Two(Two),
+    Three(Three),
 }
 
 fn main() {
@@ -53,6 +141,47 @@ fn main() {
             [F, T, T],
         ],
     };
-    let cop = [[F, T], [T, T]];
-    println!("Are we matched: {:?}", two.compare(cop));
+    let cop2 = [[F, T], [T, T]];
+
+    let three = Three {
+        pattern: [
+            [T, F, T],
+            [F, T, F],
+            [T, T, T],
+        ],
+        result: [
+            [T, T, T, F],
+            [F, F, T, T],
+            [T, T, T, F],
+            [F, F, T, T],
+        ]
+    };
+    let cop3 = [
+        [T, T, T],
+        [F, F, F],
+        [T, F, T],
+    ];
+    println!("Are we matched: {:?}", two.compare(cop2));
+    println!("Are we matched: {:?}", three.compare(cop3));
+
+    let array: Vec<Vec<Vec<Vec<bool>>>> =
+      get_str_array_from_file(&vec!{"aoc2017", "day21_data.txt"})
+        .iter()
+        .map(|x| {
+            x.split("=>").map(|v| {
+                v.trim().split("/").map(|i| {
+                    i.split("").filter(|s| !s.is_empty()).map(|j| {
+                        j == "#"
+                    }).collect()
+                }).collect()
+            }).collect()
+        }).collect();
+    let rules: Vec<Rule> = array.iter().map(|x| {
+        match x[0].len() {
+            2 => Rule::Two(Two::new(x)),
+            3 => Rule::Three(Three::new(x)),
+            _ => panic!()
+        }
+    }).collect();
+    println!("{:?}", rules);
 }
