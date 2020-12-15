9cb3e0842fb90ce42056ce67e94369ef069cd33e
msyfls123 <msyfls123@gmail.com>
2020-12-16 00:39:39 +0800

    advent_of_code 2018-7: part 1

--- advent_of_code/src/aoc2018/7.rs
+++ advent_of_code/src/aoc2018/7.rs
@@ -3,6 +3,7 @@ extern crate regex;
 
 use advent_of_code::get_str_array_from_file;
 use regex::{ Regex };
+use std::collections::HashMap;
 
 type Instruction = [char; 2];
 
@@ -16,8 +17,41 @@ fn get_instruction(text: &str) -> Instruction {
   [before, after]
 }
 
+fn generate_order(
+  map: &mut HashMap<char, Vec<char>>,
+  list: &mut Vec<char>
+) -> Vec<char> {
+  if map.keys().len() > 0 {
+    let current = map.iter().filter_map(|x| {
+      if x.1.len() == 0 {
+        Some(*x.0)
+      } else {
+        None
+      }
+    }).min().unwrap();
+    list.push(current);
+    map.remove(&current);
+    map.iter_mut().for_each(|(_, value)| {
+      if value.contains(&current) {
+        value.retain(|&x| x != current);
+      }
+    });
+    generate_order(map, list)
+  } else {
+    list.to_vec()
+  }
+}
+
 fn main() {
   let array = get_str_array_from_file(&vec!{"aoc2018", "data", "7.txt"});
   let instructions: Vec<Instruction> = array.iter().map(|t| get_instruction(t)).collect();
-  println!("{:?}", instructions);
+
+  let mut map: HashMap<char, Vec<char>> = HashMap::new();
+  instructions.iter().for_each(|&[before, after]| {
+    map.entry(before).or_insert(vec!{});
+    let after_entry = map.entry(after).or_insert(vec!{});
+    (*after_entry).push(before);
+  });
+  let order: String = generate_order(&mut map.clone(), &mut vec!{}).iter().collect();
+  println!("Part 1: the steps in my instructions be completed in order of {}", order);
 }
