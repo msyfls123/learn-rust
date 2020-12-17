354dc836e8b6b6531c469d1c9fb6ab09afd6e39a
msyfls123 <msyfls123@gmail.com>
2020-12-18 00:38:25 +0800

    advent_of_code 2018-7: part 2

--- advent_of_code/src/aoc2018/7.rs
+++ advent_of_code/src/aoc2018/7.rs
@@ -42,6 +42,50 @@ fn generate_order(
   }
 }
 
+fn parallel(
+  map: &mut HashMap<char, Vec<char>>,
+  running: Vec<(char, usize)>,
+  time: usize,
+  limit: usize,
+  extra_duration: usize,
+) -> usize {
+  let mut new_running = running.clone();
+  // remove runned from map
+  new_running = new_running.iter().filter(|&(current, count)| {
+    let result = *count != 0;
+    if !result {
+      map.remove(current);
+      map.iter_mut().for_each(|(_, value)| {
+        if value.contains(current) {
+          value.retain(|&x| x != *current);
+        }
+      });
+    }
+    result
+  }).map(|x| *x).collect();
+  // there are nothing left, return time
+  if map.keys().len() + new_running.len() == 0 {
+    return time
+  }
+  let mut waiting: Vec<char> = map.iter().filter_map(|x| {
+    if x.1.len() == 0 && !new_running.iter().any(|&y| y.0 == *x.0) {
+      Some(*x.0)
+    } else {
+      None
+    }
+  }).collect();
+  waiting.sort();
+  // count rest number for running
+  let rest = (limit - new_running.len()).min(waiting.len());
+  // push waiting to running list
+  &waiting[0..rest].iter().for_each(|&current| {
+    new_running.push((current, current as usize - 64 + extra_duration));
+  });
+  // run every running (count--)
+  new_running = new_running.iter().map(|&(step, count)| (step, count- 1)).collect();
+  parallel(map, new_running, time + 1, limit, extra_duration)
+}
+
 fn main() {
   let array = get_str_array_from_file(&vec!{"aoc2018", "data", "7.txt"});
   let instructions: Vec<Instruction> = array.iter().map(|t| get_instruction(t)).collect();
@@ -52,6 +96,9 @@ fn main() {
     let after_entry = map.entry(after).or_insert(vec!{});
     (*after_entry).push(before);
   });
+  let map = map;
   let order: String = generate_order(&mut map.clone(), &mut vec!{}).iter().collect();
-  println!("Part 1: the steps in my instructions be completed in order of {}", order);
+  println!("Part 1: the steps in my instructions be completed in order of {}.", order);
+  let duration = parallel(&mut map.clone(), vec!{}, 0, 5, 60);
+  println!("Part 2: it will take {} seconds to complete all of the steps.", duration)
 }
