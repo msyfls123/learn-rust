2a89a084e00b024c36fa1f10724ac4e365e4227a
msyfls123 <msyfls123@gmail.com>
2020-12-26 01:18:14 +0800

    advent_of_code 2018-9: part 2

--- advent_of_code/src/aoc2018/9.rs
+++ advent_of_code/src/aoc2018/9.rs
@@ -1,54 +1,49 @@
+#![feature(linked_list_cursors)]
+use std::collections::linked_list::{LinkedList, CursorMut};
 use std::collections::HashMap;
 
-fn get_position(
-  size: usize,
-  start: i32,
-  step: i32,
-  to_insert: bool,
-) -> usize {
-  let size_i32 = size as i32;
-  let dist = start + step;
-  let check = if to_insert {
-    dist > 0 && dist <= size_i32
+fn move_cursor<T>(cursor: &mut CursorMut<T>, next: bool) {
+  if next {
+    cursor.move_next();
   } else {
-    dist >= 0 && dist < size_i32
-  };
-  if check {
-    dist as usize
-  } else {
-    if dist < 0 {
-      get_position(size, dist + size_i32, 0, to_insert)
-    } else {
-      get_position(size, dist - size_i32, 0, to_insert)
+    cursor.move_prev();
+  }
+  match cursor.current() {
+    Some(_) => (),
+    None => {
+      if next {
+        cursor.move_next();
+      } else {
+        cursor.move_prev();
+      }
     }
   }
 }
 
-fn get_score(count: usize, last: usize) -> usize {
-  let mut circle = vec!{0};
-  let mut play_id = 0;
+fn get_score(players_count: usize, last: usize) -> usize {
+  let mut list: LinkedList<usize> = LinkedList::new();
   let mut marble = 0;
-  let mut index = 0;
   let mut scores: HashMap<usize, usize> = HashMap::new();
+  let mut play_id = 0;
+  list.push_back(marble);
+  let mut cursor = list.cursor_back_mut();
   loop {
-    let len = circle.len();
-    if len == 0 || marble == last {
+    if marble == last {
       break
     }
-    play_id = (play_id + 1) % count;
+    play_id = (play_id + 1) % players_count;
     marble += 1;
     if marble % 23 == 0 {
-      index = get_position(len, index as i32, -7, false);
-      let removed = circle.remove(index);
-      if index == len - 1 {
-        println!("last");
-        index = 0;
-      }
+      (0..7).for_each(|_x| {
+        move_cursor(&mut cursor, false);
+      });
+      let removed = cursor.remove_current().unwrap();
       let entry = scores.entry(play_id).or_insert(0);
       *entry += removed + marble;
     } else {
-      index = get_position(len, index as i32, 2, true);
-      circle.insert(index, marble);
+      move_cursor(&mut cursor, true);
+      cursor.insert_after(marble);
+      move_cursor(&mut cursor, true);
     }
   }
   let high_score = scores.iter().map(|s| s.1).max().unwrap();
@@ -62,4 +57,5 @@ fn main() {
   println!("{}", get_score(21, 6111));
   println!("{}", get_score(30, 5807));
   println!("Part 1: the winning Elf's score is {}.", get_score(424, 71144));
+  println!("Part 2: the new winning Elf's score be if the number of the last marble were 100 times larger is {}.", get_score(424, 7114400));
 }
