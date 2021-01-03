546e52190f95340fcf2b776936a474f46551198c
msyfls123 <msyfls123@gmail.com>
2021-01-03 22:04:44 +0800

    advent_of_code 2018-11: part 2

--- advent_of_code/src/aoc2018/11.rs
+++ advent_of_code/src/aoc2018/11.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 
 static PUZZLE_INPUT: i32 = 9995;
 static LENGTH: usize = 300;
@@ -33,4 +34,44 @@ fn main() {
     acc
   });
   println!("Part 1: the X,Y,size identifier of the square with the largest total power is {:?}.", result.0.unwrap());
+
+  let mut sum_table: HashMap<(usize, usize), i32> = HashMap::new();
+
+  (0..LENGTH).for_each(|y| {
+    (0..LENGTH).for_each(|x| {
+      let sum: i32;
+      if x == 0 && y == 0 {
+        sum = grid[0][0];
+      } else if x == 0 {
+        sum = grid[y][0] + sum_table.get(&(y - 1, 0)).unwrap();
+      } else if y == 0 {
+        sum = grid[0][x] + sum_table.get(&(0, x - 1)).unwrap();
+      } else {
+        sum = grid[y][x]
+          - sum_table.get(&(y - 1, x - 1)).unwrap()
+          + sum_table.get(&(y, x - 1)).unwrap()
+          + sum_table.get(&(y - 1, x)).unwrap();
+      }
+      sum_table.entry((y, x)).or_insert(sum);
+    })
+  });
+
+  // Really slow...
+
+  let result: (Option<(usize, usize, usize)>, i32) = (0..usize::pow(LENGTH, 2)).fold((None, 0), |mut acc, index| {
+    let x = index % LENGTH;
+    let y = index / LENGTH;
+    let max_size = usize::min(LENGTH - 1 - x, LENGTH - 1 - y);
+    (0..=max_size).for_each(|size| {
+      let power = sum_table.get(&(y + size, x + size)).unwrap()
+        + sum_table.get(&(y, x)).unwrap()
+        - sum_table.get(&(y, x + size)).unwrap()
+        - sum_table.get(&(y + size, x)).unwrap();
+      if power > acc.1 {
+        acc = (Some((x + 2, y + 2, size)), power);
+      };
+    });
+    acc
+  });
+  println!("Part 2: the X,Y,size identifier of the square with the largest total power is {:?}.", result.0.unwrap());
 }
