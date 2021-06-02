dffa37f3af56040c4f64ddc81b4230c0e5335029
msyfls123 <msyfls123@gmail.com>
2021-06-02 22:41:23 +0800

    advent_of_code 2020-2: part 2

--- advent_of_code/src/aoc2020/2.rs
+++ advent_of_code/src/aoc2020/2.rs
@@ -4,7 +4,14 @@ extern crate regex;
 use regex::{ Regex };
 use advent_of_code::get_str_array_from_file;
 
-fn check_policy(text: &str) -> bool {
+struct Policy {
+  min: usize,
+  max: usize,
+  letter: char,
+  password: String
+}
+
+fn get_policy(text: &str) -> Policy {
   lazy_static! {
     static ref RE_PASSWORD: Regex = Regex::new(
       r"(?P<min>\d+)-(?P<max>\d+)\s(?P<letter>[a-z]):\s(?P<password>[a-z]+)"
@@ -17,13 +24,32 @@ fn check_policy(text: &str) -> bool {
   let letter: char = captured.name("letter").unwrap().as_str().chars().nth(0).unwrap();
   let password = captured.name("password").unwrap().as_str().to_string();
 
-  let count = password.chars().filter(|&x| x == letter).count();
-  count >= min && count <= max
+  Policy {
+    min,
+    max,
+    letter,
+    password,
+  }
+}
+
+fn check_in_range(policy: &Policy) -> bool {
+  let Policy { password, min, max, letter } = policy;
+  let count = password.chars().filter(|x| x == letter).count();
+  count >= *min && count <= *max
+}
+
+fn check_in_position(policy: &Policy) -> bool {
+  let Policy { password, min, max, letter } = policy;
+  let chars = password.chars();
+  (chars.clone().nth(*min - 1).unwrap() == *letter) ^ (chars.clone().nth(*max - 1).unwrap() == *letter)
 }
 
 fn main() {
   let password_list = get_str_array_from_file(&vec!{"aoc2020", "data", "2.txt"});
-  let valid_passwords: Vec<String> = password_list.iter().filter(|x| check_policy(x))
+  let valid_passwords: Vec<String> = password_list.iter().filter(|x| check_in_range(&get_policy(x)))
     .map(|x| x.to_owned()).collect();
   println!("Part 1: {}", valid_passwords.len());
+  let valid_passwords: Vec<String> = password_list.iter().filter(|x| check_in_position(&get_policy(x)))
+    .map(|x| x.to_owned()).collect();
+  println!("Part 2: {}", valid_passwords.len());
 }
