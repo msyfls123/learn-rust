cfe9165343ec385681b607036f271944e8578180
msyfls123 <msyfls123@gmail.com>
2021-06-22 00:56:24 +0800

    advent_of_code 2020-6: part 2

--- advent_of_code/src/aoc2020/6.rs
+++ advent_of_code/src/aoc2020/6.rs
@@ -1,11 +1,13 @@
 use itertools::Itertools;
 use advent_of_code::get_str_from_file;
 
+const MAX: u32 = 2u32.pow(26) - 1;
+
 fn main() {
   let data: Vec<String> = get_str_from_file(&vec!{"aoc2020", "data", "6.txt"})
     .lines()
     .map(|line| line.to_string()).collect();
-  let answers: Vec<Vec<bool>> = data.into_iter()
+  let groups: Vec<Vec<String>> = data.into_iter()
     .group_by(|line| line == "")
     .into_iter()
     .filter_map(|(_key, group)| {
@@ -15,6 +17,8 @@ fn main() {
       }
       Some(lines)
     })
+    .collect();
+  let answers: Vec<Vec<bool>> = groups.iter()
     .map(|lines| {
       let mut questions = vec!{false; 26};
       lines.iter().for_each(|line| {
@@ -29,4 +33,20 @@ fn main() {
       answer.iter().filter(|&x| *x).count()
     }).sum();
     println!("Part 1: the sum of those counts is {}.", sum_of_counts);
+  let concrete_answers: Vec<u32> = groups.iter()
+    .map(|lines| {
+      let mut answer = MAX;
+      lines.iter().for_each(|line| {
+        let one_answer = line.chars().fold(0u32, |acc, c| {
+          acc | (1 << (c as u32 - 97))
+        });
+        answer &= one_answer
+      });
+      answer
+    })
+    .collect();
+  let sum_of_counts: usize = concrete_answers.iter().map(|answer| {
+    format!("{:b}", answer).chars().filter(|&x| x == '1').count()
+  }).sum();
+  println!("Part 2: the sum of those counts which everyone answered 'yes' is {}", sum_of_counts);
 }
