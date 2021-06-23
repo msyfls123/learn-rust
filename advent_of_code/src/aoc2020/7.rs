baa748663da8e2c23b5917cdc9c35811b56899c5
msyfls123 <msyfls123@gmail.com>
2021-06-23 23:24:53 +0800

    advent_of_code 2020-7: part 1

--- advent_of_code/src/aoc2020/7.rs
+++ advent_of_code/src/aoc2020/7.rs
@@ -4,11 +4,16 @@ extern crate regex;
 use regex::{ Regex };
 use advent_of_code::get_str_array_from_file;
 use itertools::Itertools;
+use std::collections::HashMap;
+
+type Rule = (String, Vec<(String, usize)>);
+
+type BagContainerMap = HashMap<String, Vec<String>>;
 
 fn get_bag(text: &str) -> (String, usize) {
   lazy_static! {
     static ref RE_BAG: Regex = Regex::new(
-      r"((?P<num>\d+)\s)?(?P<color>[a-z\s]+)(?:bags?)"
+      r"((?P<num>\d+)\s)?(?P<color>[a-z\s]+[a-z])\s(?:bags?)"
     ).unwrap();
   }
 
@@ -21,7 +26,7 @@ fn get_bag(text: &str) -> (String, usize) {
   (color, num)
 }
 
-fn get_rule(text: &str) -> (String, Vec<(String, usize)>) {
+fn get_rule(text: &str) -> Rule {
   let (master, slaves): (String, String) = text.trim_end_matches(".")
     .split(" contain ").map(|x| x.to_owned()).collect_tuple().unwrap();
   let (master_color, _) = get_bag(&master);
@@ -31,8 +36,38 @@ fn get_rule(text: &str) -> (String, Vec<(String, usize)>) {
   (master_color, slave_bags)
 }
 
+fn get_container_bags(
+  map: &BagContainerMap,
+  color: &str
+) -> Vec<String> {
+  match map.get(color) {
+    Some(containers) => {
+      let mut children: Vec<String> = containers.iter().flat_map(|c| {
+        let mut children = get_container_bags(&map, c);
+        children.push(c.to_string());
+        children
+      }).collect();
+      children.sort();
+      children.dedup();
+      children
+    },
+    None => vec!{}
+  }
+}
+
 fn main() {
-  let data = get_str_array_from_file(&vec!{"aoc2020", "data", "7.txt"});
-  let lines: Vec<(String, Vec<(String, usize)>)> = data.iter().map(|line| get_rule(&line)).collect();
-  println!("{:?}", lines);
+  let lines = get_str_array_from_file(&vec!{"aoc2020", "data", "7.txt"});
+  let rules: Vec<Rule> = lines.iter().map(|line| get_rule(&line)).collect();
+  let mut bag_container_map: BagContainerMap = HashMap::new();
+  rules.iter().for_each(|rule| {
+    let (master_color, slave_bags) = rule;
+    slave_bags.iter().for_each(|(slave, _)| {
+      let entry = bag_container_map.entry(slave.to_string()).or_insert(vec!{});
+      (*entry).push(master_color.to_string());
+    })
+  });
+  println!("{:?}", bag_container_map);
+  let result = get_container_bags(&bag_container_map, "shiny gold");
+
+  println!("{}", result.len());
 }
