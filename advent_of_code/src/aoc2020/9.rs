b33d3c713cd6da33d5aaf98b2f235bd58ce6b0a6
msyfls123 <msyfls123@gmail.com>
2021-07-01 01:05:35 +0800

    advent_of_code 2020-9: part 2

--- advent_of_code/src/aoc2020/9.rs
+++ advent_of_code/src/aoc2020/9.rs
@@ -13,6 +13,16 @@ fn check_sum(list: &[i64], next_num: i64) -> bool {
   })
 }
 
+fn check_difference(list: &[i64], next_num: i64) -> Option<(usize, usize)> {
+  let plus_list: Vec<i64> = list.iter().map(|x| next_num + x).collect();
+  list.iter().enumerate().find_map(|(index, num)| {
+    match &plus_list[0..index].iter().position(|x| x == num) {
+      Some(plus_index) => Some((plus_index + 1, index)),
+      None => None
+    }
+  })
+}
+
 fn main() {
   let list: Vec<i64> = get_str_array_from_file(&vec!{"aoc2020", "data", "9.txt"})
     .iter()
@@ -27,6 +37,19 @@ fn main() {
       false => Some(next_num),
       true => None
     }
-  });
-  println!("Part 1: {:?}", weakness);
+  }).unwrap();
+  println!("Part 1: {}", weakness);
+  let sum_list: Vec<i64> = list.iter().scan(0, |state, &x| {
+    *state += x;
+    Some(*state)
+  }).collect();
+  match check_difference(&sum_list, weakness) {
+    Some((lower_index, upper_index)) => {
+      let slice = &list[lower_index..=upper_index];
+      let smallest = slice.iter().min().unwrap();
+      let largest = slice.iter().max().unwrap();
+      println!("Part 2: {}", smallest + largest);
+    },
+    None => {},
+  };
 }
