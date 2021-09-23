f8264c00a0bdfca4016d0db84a172d2d8f677a46
msyfls123 <msyfls123@gmail.com>
2021-09-23 23:44:44 +0800

    advent_of_code 2020-19: part 2

--- advent_of_code/src/aoc2020/19.rs
+++ advent_of_code/src/aoc2020/19.rs
@@ -7,17 +7,17 @@ use std::cell::RefCell;
 use std::rc::Rc;
 use std::{collections::HashMap, hash::Hash};
 
-type RuleMap = HashMap<usize, Rule>;
+type RuleMap = HashMap<usize, RuleRaw>;
 type RuleLetters = Vec<String>;
 type CachedRuleLettersMap = HashMap<usize, RuleLetters>;
 
 #[derive(Debug)]
-enum Rule {
+enum RuleRaw {
   Pairs(Vec<Vec<usize>>),
   Letter(char)
 }
 
-fn get_rule(text: &str) -> (usize, Rule) {
+fn get_rule(text: &str) -> (usize, RuleRaw) {
   lazy_static! {
     static ref RE_PAIRS: Regex = Regex::new(r#"^(?P<index>\d+):\s(?P<pairs>.*)"#).unwrap();
     static ref RE_LETTER: Regex = Regex::new(r#"(?P<index>\d+):\s"(?P<letter>[ab])""#).unwrap();
@@ -26,7 +26,7 @@ fn get_rule(text: &str) -> (usize, Rule) {
     Some(cap) => {
       let index = cap.name("index").unwrap().as_str().parse().unwrap();
       let letter = cap.name("letter").unwrap().as_str().chars().nth(0).unwrap();
-      return (index, Rule::Letter(letter))
+      return (index, RuleRaw::Letter(letter))
     },
     None => {}
   };
@@ -42,7 +42,7 @@ fn get_rule(text: &str) -> (usize, Rule) {
           }
         }).collect()
       }).collect();
-      (index, Rule::Pairs(pairs))
+      (index, RuleRaw::Pairs(pairs))
     },
     None => panic!("noop")
   }
@@ -61,10 +61,10 @@ fn get_rule_letters(
   };
   let rule = map.get(&index).unwrap();
   match rule {
-    &Rule::Letter(c) => {
+    &RuleRaw::Letter(c) => {
       vec!{c.to_string()}
     },
-    Rule::Pairs(pairs) => {
+    RuleRaw::Pairs(pairs) => {
       let letters: Vec<String> = pairs.iter().flat_map(|pair| {
         pair.iter().fold(vec!{String::from("")}, |acc: Vec<String>, &i| {
           let sub_letters = get_rule_letters(map, cached.clone(), i);
@@ -83,18 +83,110 @@ fn get_rule_letters(
   }
 }
 
-fn main() {
-  let data = get_group_str_from_file(&vec!{"aoc2020", "data", "19.txt"});
-  let raw_rules = &data[0];
-  let messages = &data[1];
+fn part1(rules: &Vec<String>, msgs: &Vec<String>) {
   let mut rule_map: RuleMap = HashMap::new();
-  raw_rules.iter().for_each(|line| {
+  rules.iter().for_each(|line| {
     let (index, rule) = get_rule(line);
     rule_map.insert(index, rule);
   });
   // println!("{:?}", rule_map);
   let cached: Rc<RefCell<CachedRuleLettersMap>> = Rc::new(RefCell::new(HashMap::new()));
   let rule_letters = get_rule_letters(&rule_map, cached.clone(), 0);
-  let matched_message_count = messages.iter().filter(|m| rule_letters.contains(m)).count();
+  let matched_message_count = msgs.iter().filter(|m| rule_letters.contains(m)).count();
   println!("Part 1: {:?}", matched_message_count);
+}
+
+/// copied from https://github.com/timvisee/advent-of-code-2020/blob/master/day19b/src/main.rs
+enum Rule {
+  Lit(u8),
+  Seq(Vec<usize>),
+  SeqOr(Vec<usize>, Vec<usize>),
+}
+
+fn matches_42(msg: &[u8], rules: &[Rule]) -> bool {
+  (0..)
+      .try_fold(msg, |msg, depth| match matches(msg, 42, rules) {
+          Some(msg) if matches_31(depth, msg, rules) => Err(true),
+          Some(msg) => Ok(msg),
+          None => Err(false),
+      })
+      .err()
+      .unwrap()
+}
+
+fn matches_31(depth: usize, msg: &[u8], rules: &[Rule]) -> bool {
+  (0..depth)
+      .try_fold(msg, |msg, _| match matches(msg, 31, rules) {
+          Some(msg) if msg.is_empty() => Err(true),
+          Some(msg) => Ok(msg),
+          None => Err(false),
+      })
+      .err()
+      .unwrap_or(false)
+}
+
+fn matches<'a>(msg: &'a [u8], rule: usize, rules: &[Rule]) -> Option<&'a [u8]> {
+  match &rules[rule] {
+      Rule::Lit(_) if msg.is_empty() => None,
+      Rule::Lit(c) if &msg[0] == c => Some(&msg[1..]),
+      Rule::Lit(_) => None,
+      Rule::Seq(a) => a.into_iter().try_fold(msg, |m, &r| matches(m, r, rules)),
+      Rule::SeqOr(a, b) => a
+          .into_iter()
+          .try_fold(msg, |m, &r| matches(m, r, rules))
+          .or_else(|| b.into_iter().try_fold(msg, |m, &r| matches(m, r, rules))),
+  }
+}
+
+fn part2(rules: &Vec<String>, msgs: &Vec<String>) {
+  let mut rules: Vec<(usize, Rule)> = rules
+        .iter()
+        .map(|l| {
+            let (n, rule) = l.split_once(": ").unwrap();
+            (
+                n.parse().unwrap(),
+                if rule.starts_with('"') {
+                    Rule::Lit(rule.chars().nth(1).unwrap() as u8)
+                } else {
+                    let parts: Vec<_> = rule.splitn(2, '|').collect();
+                    let a = parts[0]
+                        .split_terminator(' ')
+                        .filter(|n| !n.is_empty())
+                        .map(|n| n.parse().unwrap())
+                        .collect();
+
+                    if parts.len() == 1 {
+                        Rule::Seq(a)
+                    } else {
+                        let b = parts[1]
+                            .split_terminator(' ')
+                            .filter(|n| !n.is_empty())
+                            .map(|n| n.parse().unwrap())
+                            .collect();
+                        Rule::SeqOr(a, b)
+                    }
+                },
+            )
+        })
+        .collect();
+    rules.sort_unstable_by_key(|r| r.0);
+    let rules: Vec<_> = rules.into_iter().map(|r| r.1).collect();
+
+    // rules[8] = Rule::SeqOr(vec![42], vec![42, 8]);
+    // rules[11] = Rule::SeqOr(vec![42, 31], vec![42, 11, 31]);
+
+    println!(
+        "Part 2: {}",
+        msgs.iter()
+            .filter(|msg| matches_42(msg.as_bytes(), &rules))
+            .count(),
+    );
+}
+
+/// copied end
+
+fn main() {
+  let data = get_group_str_from_file(&vec!{"aoc2020", "data", "19.txt"});
+  part1(&data[0], &data[1]);
+  part2(&data[0], &data[1]);
 }
