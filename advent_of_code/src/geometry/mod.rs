87193429ec359f2079d0f26bbb17b501a1f96e3c
msyfls123 <msyfls123@gmail.com>
2021-11-16 23:53:43 +0800

    geometry: add flip method.

--- advent_of_code/src/geometry/mod.rs
+++ advent_of_code/src/geometry/mod.rs
@@ -2,6 +2,11 @@ use itertools::Itertools;
 
 type Matrix<T> = Vec<Vec<T>>;
 
+pub enum FlipAxis {
+  Horizontal,
+  Vertical,
+}
+
 pub fn clockwise<T: Clone>(matrix: &Matrix<T>, opposite: bool) -> Matrix<T> {
   let len_x = matrix[0].len();
   let len_y = matrix.len();
@@ -17,6 +22,19 @@ pub fn clockwise<T: Clone>(matrix: &Matrix<T>, opposite: bool) -> Matrix<T> {
         matrix[len_y - 1 - x][y].to_owned()
       }
     }).collect()
+  }).collect_vec()
+}
+
+pub fn flip<T: Clone>(matrix: &Matrix<T>, axis: FlipAxis) -> Matrix<T>{
+  let len_x = matrix[0].len();
+  let len_y = matrix.len();
+  (0..len_y).map(|y| {
+    (0..len_x).map(|x| {
+      match axis {
+        FlipAxis::Horizontal => matrix[len_y - 1 - y][x].to_owned(),
+        FlipAxis::Vertical => matrix[y][len_x - 1 - x].to_owned(),
+      }
+    }).collect_vec()
   }).collect_vec()
 }
 
@@ -47,6 +65,33 @@ mod matrix {
     assert_eq!(
       target_opposite,
       clockwise(&origin, true)
+    );
+  }
+
+  #[test]
+  fn test_flip() {
+    let origin = vec!{
+      vec!{1, 2, 3},
+      vec!{4, 5, 6},
+      vec!{7, 8, 9},
+    };
+    let target_horizontal = vec!{
+      vec!{7, 8, 9},
+      vec!{4, 5, 6},
+      vec!{1, 2, 3},
+    };
+    let target_vertical = vec!{
+      vec!{3, 2, 1},
+      vec!{6, 5, 4},
+      vec!{9, 8, 7},
+    }; 
+    assert_eq!(
+      target_horizontal,
+      flip(&origin, FlipAxis::Horizontal)
+    );
+    assert_eq!(
+      target_vertical,
+      flip(&origin, FlipAxis::Vertical)
     );
   }
 }
