a001cbb6df78721f242726589921ef13807938f4
msyfls123 <msyfls123@gmail.com>
2022-06-05 13:17:53 +0800

    advent_of_code 2021-1: part 2

--- advent_of_code/src/aoc2021/1.rs
+++ advent_of_code/src/aoc2021/1.rs
@@ -1,10 +1,8 @@
 
 use advent_of_code::get_str_array_from_file;
 
-fn main() {
-    let data = get_str_array_from_file(&vec!{"aoc2021", "data", "1.txt"});
-    let nums: Vec<usize> = data.iter().map(|x| x.parse().unwrap()).collect();
-    let increases_sum = nums.iter().scan(None, |prev, x| {
+fn get_increase_sum(nums: &Vec<usize>) -> usize {
+    nums.iter().scan(None, |prev, x| {
         match *prev {
             Some(prev_num) => {
                 let result = x > prev_num;
@@ -16,6 +14,19 @@ fn main() {
                 Some(false)
             }
         }
-    }).filter(|x| *x).count();
+    }).filter(|x| *x).count()
+}
+
+fn main() {
+    let data = get_str_array_from_file(&vec!{"aoc2021", "data", "1.txt"});
+    let nums: Vec<usize> = data.iter().map(|x| x.parse().unwrap()).collect();
+    let increases_sum = get_increase_sum(&nums);
     println!("Part 1: {}", increases_sum);
+
+    let sliding_windows: Vec<usize> = (0..nums.len() - 2).map(|x| {
+        nums[x] + nums[x+1] + nums[x+2]
+    }).collect();
+
+    let increases_sum = get_increase_sum(&sliding_windows);
+    println!("Part 2: {}", increases_sum);
 }
