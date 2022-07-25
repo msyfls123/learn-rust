f9b4e61dd312769bb82ba212c97708094764932d
msyfls123 <msyfls123@gmail.com>
2022-07-26 00:33:25 +0800

    advent_of_code 2021-3: part 2

--- advent_of_code/src/aoc2021/3.rs
+++ advent_of_code/src/aoc2021/3.rs
@@ -5,13 +5,43 @@ use itertools::Itertools;
 
 type CommonBitMap = HashMap<usize, isize>;
 
+fn find_rating(
+    bit_lists: &Vec<Vec<usize>>,
+    index: usize,
+    is_most: bool
+) -> Vec<Vec<usize>> {
+    if bit_lists.len() == 1 {
+        return bit_lists.to_owned()
+    }
+    let zeroes = bit_lists.iter().filter_map(|bits| if bits[index] == 0 {
+        Some(bits.to_owned())
+    } else { None }).collect_vec();
+    let ones = bit_lists.iter().filter_map(|bits| if bits[index] == 1 {
+        Some(bits.to_owned())
+    } else { None }).collect_vec();
+    let target = if is_most {
+        if ones.len() >= zeroes.len() {
+            ones
+        } else {
+            zeroes
+        }
+    } else {
+        if zeroes.len() <= ones.len() {
+            zeroes
+        } else {
+            ones
+        }
+    };
+    find_rating(&target.to_owned(), index + 1, is_most)
+}
+
 fn main() {
     let data = get_str_array_from_file(&vec!{"aoc2021", "data", "3.txt"});
     let numbers  = data.iter().map(|line| {
         line.split("").filter(|&x| x != "").map(|c| c.parse::<usize>().unwrap()).collect_vec()
     }).collect_vec();
     let mut common_map: CommonBitMap = HashMap::new();
-    for nums in numbers {
+    for nums in numbers.clone() {
         for  (index, &num) in nums.iter().enumerate() {
             let entry = common_map.entry(index).or_insert(0);
             if num == 0 {
@@ -29,4 +59,10 @@ fn main() {
     let least_num = isize::from_str_radix(&least_common.iter().join(""), 2).unwrap();
     let power_comsumption = most_num * least_num;
     println!("Part 1: {}", power_comsumption);
+
+    let oxygen_rating = find_rating(&numbers.clone(), 0, true);
+    let co2_rating = find_rating(&numbers.clone(), 0, false);
+    let oxygen_num = isize::from_str_radix(&oxygen_rating.first().unwrap().iter().join(""), 2).unwrap();
+    let co2_num = isize::from_str_radix(&co2_rating.first().unwrap().iter().join(""), 2).unwrap();
+    println!("Part 2: {}", oxygen_num * co2_num);
 }
