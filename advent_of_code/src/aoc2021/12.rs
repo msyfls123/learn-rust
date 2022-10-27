8776df7c92dc0d96890b22c2606c9950dcde4c93
msyfls123 <msyfls123@gmail.com>
2022-10-28 00:29:29 +0800

    advent_of_code 2021-12: part 2

--- advent_of_code/src/aoc2021/12.rs
+++ advent_of_code/src/aoc2021/12.rs
@@ -18,19 +18,29 @@ fn get_map(lines: &Vec<String>) -> CaveMap {
   map
 }
 
-fn visit(map: &CaveMap, caves: &Vec<String>, visited: &Vec<String>) -> Vec<Vec<String>> {
+/// * `is_second_life` - A boolean value means whether meet small cave before
+fn visit(map: &CaveMap, caves: &Vec<String>, visited: &Vec<String>, is_second_life: bool) -> Vec<Vec<String>> {
   caves.iter().flat_map(|cave| {
+    let mut is_second_life = is_second_life;
+    if cave.chars().next().unwrap().is_lowercase() && visited.contains(cave) {
+      if is_second_life {
+        return vec!{};
+      } else {
+        is_second_life = true;
+      }
+    }
+
     if *cave == END {
       vec!{[visited.to_owned(), vec!{END.to_string()}].concat()}
     } else {
       let next_caves: Vec<String> = map.get(cave).unwrap().iter().filter_map(|c| {
-        if c == START || (c.chars().next().unwrap().is_lowercase() && visited.contains(&c)) {
+        if c == START {
           None
         } else {
           Some(c.to_owned())
         }
       }).collect();
-      visit(map, &next_caves, &([visited.to_owned(), vec!{cave.to_owned()}].concat()))
+      visit(map, &next_caves, &([visited.to_owned(), vec!{cave.to_owned()}].concat()), is_second_life)
     }
   }).collect()
 }
@@ -47,8 +57,10 @@ fn test_visit() {
     "b-end",
   }.iter().map(|v| v.to_string()).collect());
 
-  let visit_pathes = visit(&map, &vec!{START.to_string()}, &vec!{});
+  let visit_pathes = visit(&map, &vec!{START.to_string()}, &vec!{}, true);
+  let visit_pathes_2 = visit(&map, &vec!{START.to_string()}, &vec!{}, false);
   assert_eq!(visit_pathes.len(), 10);
+  assert_eq!(visit_pathes_2.len(), 36);
 
   let map = get_map(&vec!{
     "fs-end",
@@ -71,8 +83,10 @@ fn test_visit() {
     "start-RW",
   }.iter().map(|v| v.to_string()).collect());
 
-  let visit_pathes = visit(&map, &vec!{START.to_string()}, &vec!{});
+  let visit_pathes = visit(&map, &vec!{START.to_string()}, &vec!{}, true);
+  let visit_pathes_2 = visit(&map, &vec!{START.to_string()}, &vec!{}, false);
   assert_eq!(visit_pathes.len(), 226);
+  assert_eq!(visit_pathes_2.len(), 3509);
 }
 
 
@@ -81,7 +95,11 @@ fn main() {
   let lines = get_str_array_from_file(&vec!{"aoc2021", "data", "12.txt"});
   let map = get_map(&lines);
 
-  let visit_pathes = visit(&map, &vec!{START.to_string()}, &vec!{});
+  let visit_pathes = visit(&map, &vec!{START.to_string()}, &vec!{}, true);
 
   println!("Part 1: {}", visit_pathes.len());
+
+  let visit_pathes_2 = visit(&map, &vec!{START.to_string()}, &vec!{}, false);
+
+  println!("Part 2: {}", visit_pathes_2.len());
 }
