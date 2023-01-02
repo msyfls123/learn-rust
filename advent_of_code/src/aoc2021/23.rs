ed7fd9f658d35f088d4c219ee6d12476ffa9a7be
msyfls123 <msyfls123@gmail.com>
2023-01-03 01:07:17 +0800

    advent_of_code 2021-23: part 2 failed

--- advent_of_code/src/aoc2021/23.rs
+++ advent_of_code/src/aoc2021/23.rs
@@ -1,6 +1,7 @@
 #![feature(generic_const_exprs)]
-use std::{convert::TryInto, fmt::Display, collections::{HashMap, BinaryHeap}, default};
+use std::{convert::TryInto, fmt::Display, collections::{HashMap, BinaryHeap}, hash::Hash};
 use chrono::{Local};
+use std::cmp::Reverse;
 
 /**
  * diagram
@@ -21,7 +22,6 @@ use chrono::{Local};
 type Hallway = [usize; 7];
 
 trait StateTrait {
-    const RoomSize: usize;
     fn possible_edge_states(&self) -> Vec<(Self, usize)> where Self: Sized;
     fn energy(&self, room_info: (usize, usize), h_idx: usize, room_to_hallway: bool) -> usize;
     fn room_to_hallway(&self, room_info: (usize, usize), h_idx: usize) -> Option<Self> where Self: Sized;
@@ -64,22 +64,26 @@ impl HasHallwayAndRooms for StateFor2 {
     }
 }
 
-#[derive(Clone, Copy, PartialEq, Eq)]
-struct Situation {
-    energy: usize,
-    state: StateFor2,
+#[derive(Debug, PartialEq, Eq, Hash, Default, Clone, Copy, PartialOrd, Ord)]
+struct StateFor4 {
+    hallway: Hallway,
+    side_rooms: [[usize; 4]; 4],
 }
 
-impl Ord for Situation {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        other.energy.cmp(&self.energy)
-            .then_with(|| self.state.cmp(&other.state))
+impl HasHallwayAndRooms for StateFor4 {
+    const SIZE: usize = 4;
+    fn get_rooms(&self) -> [[usize; Self::SIZE]; 4] {
+        self.side_rooms
     }
-}
-
-impl PartialOrd for Situation {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
+    fn get_hallway(&self) -> Hallway {
+        self.hallway
+    }
+    fn new(hallway: Hallway, rooms: &Vec<Vec<usize>>) -> Self {
+        Self {
+            hallway,
+            side_rooms: rooms.iter().map(|v| v.clone().try_into().expect("side_room collect fail"))
+                .collect::<Vec<[usize; Self::SIZE]>>().try_into().expect("side_room collect fail"),
+        }
     }
 }
 
@@ -120,37 +124,72 @@ impl Display for StateFor2 {
     }
 }
 
+impl Display for StateFor4 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "#############\n")?;
+        write!(
+            f,
+            "#{}{}.{}.{}.{}.{}{}#\n",
+            self.hv(0),
+            self.hv(1),
+            self.hv(2),
+            self.hv(3),
+            self.hv(4),
+            self.hv(5),
+            self.hv(6),
+        )?;
+        for i in (1..=4) {
+            write!(
+                f,
+                "###{}#{}#{}#{}###\n",
+                self.rv(0, 4 - i),
+                self.rv(1, 4 - i),
+                self.rv(2, 4 - i),
+                self.rv(3, 4 - i),
+            )?;
+        }
+        write!(
+            f,
+            "  #########\n",
+        )
+    }
+}
+
 impl <T: HasHallwayAndRooms + Clone> StateTrait for T where [(); Self::SIZE]: {
-    const RoomSize: usize = Self::SIZE;
     fn possible_edge_states(&self) -> Vec<(Self, usize)> {
-        let hallway_states: Vec<(Self, usize)> = self.get_hallway().iter().enumerate().flat_map(|(h_idx, &value)| {
-            if value == 0 { return vec!{}; }
-            let rooms: [[usize; Self::SIZE]; 4] = self.get_rooms();
-            rooms.iter().enumerate().flat_map(|(r_idx, room)| {
-                room.iter().enumerate().filter_map(move |(a_pos, &r_value)| {
-                    if r_value != 0 { return None; }
-                    // not destination room
-                    if r_idx + 1 != value { return None; }
-                    self.hallway_to_room(h_idx, (r_idx, a_pos)).map(|state| (
-                        state,
-                        self.energy((r_idx, a_pos), h_idx, false)
-                    ))
-                })
-            }).collect()
-        }).collect();
-        let room_states: Vec<(Self, usize)> = self.get_rooms().iter().enumerate().flat_map(|(r_idx, room)| {
-            room.iter().enumerate().flat_map(|(a_pos, &r_value)| {
-                if r_value == 0 { return vec!{}; }
-                self.get_hallway().iter().enumerate().filter_map(|(h_idx, &h_value)| {
-                    if h_value != 0 { return None };
-                    self.room_to_hallway((r_idx, a_pos), h_idx).map(|state| (
-                        state,
-                        self.energy((r_idx, a_pos), h_idx, true)
-                    ))
-                }).collect()
-            }).collect::<Vec<(Self, usize)>>()
-        }).collect();
-        [hallway_states, room_states].concat()
+        let mut states = vec!{};
+        self.get_hallway().iter().enumerate().for_each(|(h_idx, &h_value)| {
+            // empty hallway
+            if h_value == 0 { return; }
+            let r_idx = h_value - 1;
+            self.get_rooms()[r_idx].iter().enumerate().any(|(a_pos, &r_value)| {
+                if r_value != 0 { return false; }
+                match self.hallway_to_room(h_idx, (r_idx, a_pos)) {
+                    Some(state) => {
+                        states.push((state, self.energy((r_idx, a_pos), h_idx, false)));
+                    },
+                    _ => {},
+                }
+                true
+            });
+        });
+        self.get_rooms().iter().enumerate().for_each(|(r_idx, room)| {
+            let mut room_iter = room.iter().enumerate().rev();
+            room_iter.any(|(a_pos, &r_value)| {
+                if r_value == 0 { return false; }
+                self.get_hallway().iter().enumerate().for_each(|(h_idx, &h_value)| {
+                    if h_value != 0 { return };
+                    match self.room_to_hallway((r_idx, a_pos), h_idx) {
+                        Some(|state) => {
+                            states.push((state, self.energy((r_idx, a_pos), h_idx, true)));
+                        },
+                        _ => {},
+                    }
+                });
+                true
+            });
+        });
+        states
     }
 
     fn energy(&self, (r_idx, a_pos): (usize, usize), h_idx: usize, room_to_hallway: bool) -> usize {
@@ -160,9 +199,9 @@ impl <T: HasHallwayAndRooms + Clone> StateTrait for T where [(); Self::SIZE]: {
             self.get_hallway()[h_idx]
         };
         let distance = match h_idx {
-            0 => (r_idx * 2 + 2) + (2 - a_pos),
-            6 => (8 - r_idx * 2) + (2 - a_pos),
-            num => ((num as isize) * 2 - (r_idx as isize) * 2 - 3).abs() as usize + (2 - a_pos)
+            0 => (r_idx * 2 + 2) + (Self::SIZE - a_pos),
+            6 => (8 - r_idx * 2) + (Self::SIZE - a_pos),
+            num => ((num as isize) * 2 - (r_idx as isize) * 2 - 3).abs() as usize + (Self::SIZE - a_pos)
         };
         usize::pow(10, (target - 1) as u32) * distance
     }
@@ -398,25 +437,33 @@ fn test_energy() {
     assert_eq!(start.energy((3, 0), 6, true), 4);
 }
 
-fn shortest_path(start: StateFor2, end: &StateFor2) -> Option<usize> {
+fn shortest_path<T: Eq + Hash + Ord + Clone + Copy + StateTrait + Display>(start: T, end: &T) -> Option<usize> {
     let mut dist = HashMap::new();
     let mut heap = BinaryHeap::new();
     dist.insert(start, 0);
-    heap.push(Situation { energy: 0, state: start });
-    while let Some(Situation { energy, state }) = heap.pop() {
+    heap.push(Reverse((0, start)));
+    let mut step = 0;
+    while let Some(item) = heap.pop() {
+        let (energy, state) = item.0;
+        step += 1;
+        // audit ...
+        if step % 10000 == 0 {
+            println!("{}Energy: {}\n\n----------\n\n", state, energy);
+        }
+
         if &state == end { return Some(energy); }
 
         if &energy > dist.get(&state).or(Some(&usize::MAX)).unwrap() { continue; }
 
         for (new_state, new_energy) in state.possible_edge_states() {
-            let next = Situation {
-                energy: new_energy + energy,
-                state: new_state,
-            };
+            let next = (
+                new_energy + energy,
+                new_state,
+            );
 
-            if &next.energy < dist.get(&next.state).or(Some(&usize::MAX)).unwrap() {
-                heap.push(next);
-                dist.insert(next.state, next.energy);
+            if &next.0 < dist.get(&next.1).or(Some(&usize::MAX)).unwrap() {
+                heap.push(Reverse(next));
+                dist.insert(next.1, next.0);
             }
         }
     }
@@ -444,8 +491,30 @@ fn main() {
             [4,4],
         ]
     };
-    let shorest_path = shortest_path(start, &end);
-    println!("{:?}", shorest_path);
+    let shorest_path1 = shortest_path(start, &end);
+    println!("Part 1: {:?}", shorest_path1);
     let duration = Local::now() - now;
     println!("Transform graph cost: {:?}", duration.to_std().unwrap());
+
+    let start = StateFor4 {
+        hallway: [0,0,0,0,0,0,0],
+        side_rooms: [
+            [4,4,4,4],
+            [3,2,3,1],
+            [2,1,2,3],
+            [2,3,1,1],
+        ]
+    };
+    let end = StateFor4 {
+        hallway: [0,0,0,0,0,0,0],
+        side_rooms: [
+            [1,1,1,1],
+            [2,2,2,2],
+            [3,3,3,3],
+            [4,4,4,4],
+        ]
+    };
+    let shorest_path2 = shortest_path(start, &end);
+    println!("Part 1: {:?}", shorest_path1);
+    println!("Part 2: {:?}", shorest_path2);
 }
