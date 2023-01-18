6b4238a6b5563f28d8bfc96ecd1cb76ace5d8ab9
msyfls123 <msyfls123@gmail.com>
2023-01-18 18:51:13 +0800

    advent_of_code 2021-24: part 1

--- advent_of_code/src/aoc2021/24.rs
+++ advent_of_code/src/aoc2021/24.rs
@@ -1,16 +1,15 @@
-use std::{collections::HashMap, convert::TryInto};
+use std::{collections::{HashSet}};
+
+use advent_of_code::get_str_array_from_file;
 
 type Unit = [isize; 4];
 
 /**
- * One inp step to next result
+ * Failed unit
  */
-type OperationCache = HashMap<Unit, Unit>;
+type Cache = HashSet<Unit>;
 
-/**
- * Every inp step to next (or final) result
- */
-type ProgramCache = HashMap<usize, OperationCache>;
+const SORTED_INPUTS: [isize;9] = [9,8,7,6,5,4,3,2,1];
 
 #[derive(Debug, PartialEq, Eq)]
 struct InstructionResult {
@@ -18,7 +17,11 @@ struct InstructionResult {
     consumed: bool
 }
 
-fn instruction(unit: Unit, text: &str, input: isize) -> InstructionResult {
+fn split(text: &str) -> Vec<&str> {
+    text.split(" ").collect()
+}
+
+fn instruction(unit: &Unit, seps: &Vec<&str>, input: isize) -> InstructionResult {
     let get = |v: &str| {
         match v.parse::<isize>() {
             Ok(num) => num,
@@ -30,16 +33,11 @@ fn instruction(unit: Unit, text: &str, input: isize) -> InstructionResult {
     };
     let set = |v: &str, data: isize| -> Unit {
         let key = (v.chars().next().unwrap() as u8 - 119) as usize;
-        unit.iter().enumerate().map(|(index, val)| {
-            if index == key {
-                data
-            } else {
-                *val
-            }
-        }).collect::<Vec<isize>>().try_into().unwrap()
+        let mut new_unit = *unit;
+        new_unit[key] = data;
+        new_unit
     };
     let mut consumed = false;
-    let seps: Vec<&str> = text.split(" ").collect();
 
     let new_unit = match seps[0] {
         "inp" => {
@@ -76,6 +74,7 @@ fn instruction(unit: Unit, text: &str, input: isize) -> InstructionResult {
         },
         _ => panic!("cannot believe it")
     };
+    // println!("{} - {}\ninput:{:#?}\noutput:{:#?}\n\n", text, input, unit, new_unit);
     InstructionResult {
         unit: new_unit,
         consumed,
@@ -85,21 +84,21 @@ fn instruction(unit: Unit, text: &str, input: isize) -> InstructionResult {
 #[test]
 fn test_instruction() {
     assert_eq!(
-        instruction([0,0,0,0], "inp x", 5),
+        instruction(&[0,0,0,0], &split("inp x"), 5),
         InstructionResult {
             unit: [0,5,0,0],
             consumed: true,
         }
     );
     assert_eq!(
-        instruction([3,1,0,0], "add x w", 5),
+        instruction(&[3,1,0,0], &split("add x w"), 5),
         InstructionResult {
             unit: [3,4,0,0],
             consumed: false,
         }
     );
     assert_eq!(
-        instruction([0,0,3,2], "mul y z", 5),
+        instruction(&[0,0,3,2], &split("mul y z"), 5),
         InstructionResult {
             unit: [0,0,6,2],
             consumed: false,
@@ -107,5 +106,53 @@ fn test_instruction() {
     );
 }
 
+fn find_sat_input(
+    instructions: &Vec<Vec<&str>>,
+    mut unit: Unit,
+    mut index: usize,
+    failed_cache: &mut [Cache],
+) -> Option<Vec<isize>> {
+    let (init_unit, init_index) = (unit, index);
+    if failed_cache[init_index].contains(&init_unit) {
+        return None
+    }
+    let total = instructions.len();
+    while index < total {
+        let instruction_text = &instructions[index];
+        let is_inp = instruction_text[0] == "inp";
+        if is_inp {
+            for input in SORTED_INPUTS {
+                InstructionResult { unit, .. } = instruction(&unit, &instruction_text, input);
+                if let Some(res) = find_sat_input(instructions, unit, index + 1, failed_cache) {
+                    return Some([vec!{input}, res].concat())
+                }
+                // debug, show first two inp results
+                if index < 20 {
+                    println!("fail {} {}", index, input);
+                }
+            }
+            // all inputs failed, mark this init_unit as invalid
+            failed_cache[init_index].insert(init_unit);
+            return None;
+        } else {
+            InstructionResult { unit, .. } = instruction(&unit, &instruction_text, 0);
+        }
+        index += 1;
+    }
+    if unit[3] == 0 {
+        Some(vec!{})
+    } else {
+        failed_cache[init_index].insert(init_unit);
+        None
+    }
+}
+
 fn main() {
+    let data = get_str_array_from_file(&vec!{"aoc2021", "data", "24.txt"});
+    let instructions: Vec<Vec<&str>> = data.iter().map(|text| text.split(" ").collect()).collect();
+    let mut cache = vec![Cache::new(); instructions.len()];
+    let first = find_sat_input(&instructions, [0,0,0,0], 0, &mut cache);
+    println!("Part 1: {:?}", first.map(|nums| {
+        nums.iter().map(|num| num.to_string()).collect::<String>()
+    }));
 }
