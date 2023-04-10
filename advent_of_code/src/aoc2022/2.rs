a44eff2236f3fc026283133a850dfdd775810331
msyfls123 <msyfls123@gmail.com>
2023-04-10 21:28:42 +0800

    advent_of_code 2022-2: part 2

--- advent_of_code/src/aoc2022/2.rs
+++ advent_of_code/src/aoc2022/2.rs
@@ -1,7 +1,7 @@
 use advent_of_code::get_str_array_from_file;
 use itertools::Itertools;
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 enum Shape {
     Rock,
     Paper,
@@ -49,6 +49,35 @@ fn test_from_string() {
 }
 
 impl Strategy {
+    fn from_round_end(text: &str) -> Self {
+        use Shape::*;
+        let (x, y): (&str, &str) = text.split_whitespace().collect_tuple().unwrap();
+        let opponent = match x {
+            "A" => Shape::Rock,
+            "B" => Shape::Paper,
+            "C" => Shape::Scissors,
+            _ => panic!("parse opponent failed")
+        };
+        let you = match y {
+            "X" => match opponent {
+                Rock => Scissors,
+                Paper => Rock,
+                Scissors => Paper,
+            },
+            "Y" => opponent,
+            "Z" => match opponent {
+                Rock => Paper,
+                Paper => Scissors,
+                Scissors => Rock,
+            },
+            _ => panic!("parse you failed")
+        };
+        Self {
+            opponent,
+            you
+        }
+    }
+
     fn shape_score(&self) -> u32 {
         match self.you {
             Shape::Rock => 1,
@@ -90,4 +119,8 @@ fn main() {
     let strategies: Vec<Strategy> = data.iter().map(|text| text.as_str().into()).collect();
     let total_score: u32 = strategies.iter().map(|s| s.score()).sum();
     println!("Part 1: {}", total_score);
+
+    let strategies: Vec<Strategy> = data.iter().map(|text| Strategy::from_round_end(text)).collect();
+    let total_score: u32 = strategies.iter().map(|s| s.score()).sum();
+    println!("Part 2: {}", total_score);
 }
