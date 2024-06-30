b733e5b0211669cd4810e891e3502cfe50623198
msyfls123 <msyfls123@gmail.com>
2024-06-30 23:56:28 +0800

    feat: aoc2022-15 part 1

---
+++ advent_of_code/src/aoc2022/15.rs
@@ -0,0 +1,77 @@
+extern crate regex;
+#[macro_use] extern crate lazy_static;
+
+use std::collections::HashSet;
+use advent_of_code::get_str_array_from_file;
+use regex::Regex;
+
+type Point = (isize, isize);
+
+struct Report {
+    sensor: (isize, isize),
+    beacon: (isize, isize),
+}
+
+fn parse(input: &str) -> Report {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"Sensor\sat\sx=([-\d]+),\sy=([-\d]+):\sclosest\sbeacon\sis\sat\sx=([-\d]+),\sy=([-\d]+)").unwrap();
+    }
+    let caps = RE.captures(input).unwrap();
+    let sensor = (caps[1].parse().unwrap(), caps[2].parse().unwrap());
+    let beacon = (caps[3].parse().unwrap(), caps[4].parse().unwrap());
+    Report { sensor, beacon }
+}
+
+#[test]
+fn test_parse() {
+    let input = "Sensor at x=2, y=18: closest beacon is at x=-2, y=15";
+    let report = parse(input);
+    assert_eq!(report.sensor, (2, 18));
+    assert_eq!(report.beacon, (-2, 15));
+}
+
+fn find_clear_points(
+    report: &Report,
+    y: isize,
+) -> Vec<Point> {
+    let Report { sensor, beacon } = report;
+    let dy = (sensor.1 - y).abs();
+    let distance = (sensor.1 - beacon.1).abs() + (sensor.0 - beacon.0).abs();
+
+    if distance < dy {
+        vec![]
+    } else {
+        let dx = distance - dy;
+        (sensor.0 - dx..=sensor.0 + dx)
+            .map(|x| (x, y))
+            .collect()
+    }
+}
+
+#[test]
+fn test_find_clear_points() {
+    let report = Report { sensor: (8,7), beacon: (2, 10) };
+    let points = find_clear_points(&report, 10);
+    assert_eq!(points, (2..=14).map(|x| (x, 10)).collect::<Vec<_>>());
+}
+
+fn find_all_clear_points(reports: &Vec<Report>, y: isize) -> HashSet<Point> {
+    let mut points: HashSet<Point> = reports.iter()
+        .flat_map(|report| {
+            find_clear_points(report, y)
+        })
+        .collect();
+    for Report { beacon, .. } in reports {
+        points.remove(beacon);
+    }
+    points
+}
+
+fn main() {
+    let data = get_str_array_from_file(&vec!["aoc2022", "data", "15.txt"]);
+    let reports: Vec<Report> = data.iter().map(|x| parse(x)).collect();
+    let clear_points = find_all_clear_points(&reports, 2_000_000);
+
+    println!("Part 1: {}", clear_points.len());
+    
+}
